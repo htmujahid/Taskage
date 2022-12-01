753248fa0790db2571ba129cd71cd16d18c304ad
Talha Mujahid <htmujahid@gmail.com>
2022-12-02 01:42:51 +0500

    initial commit

--- components/Form/FormWrapper.jsx
+++ components/Form/FormWrapper.jsx
@@ -12,7 +12,7 @@ function FormWrapper({ type, children, className }) {
     else if (type === 1)
         return (
             <div
-                className={`flex flex-col sm:flex-row justify-between items-center gap-2 ${className}`}
+                className={`flex flex-col sm:flex-row justify-between items-center gap-x-3 ${className}`}
             >
                 {children}
             </div>
@@ -20,7 +20,7 @@ function FormWrapper({ type, children, className }) {
     else if (type === 2)
         return (
             <div
-                className={`flex justify-between items-center gap-2 ${className}`}
+                className={`flex justify-between items-center gap-x-3 ${className}`}
             >
                 {children}
             </div>
@@ -28,7 +28,23 @@ function FormWrapper({ type, children, className }) {
     else if (type === 3)
         return (
             <div
-                className={`flex justify-center items-center gap-2 ${className}`}
+                className={`flex justify-center items-center gap-x-3 ${className}`}
+            >
+                {children}
+            </div>
+        );
+    else if (type === 4)
+        return (
+            <div
+                className={`flex flex-col sm:flex-row justify-between items-center gap-x-3 gap-y-3 ${className}`}
+            >
+                {children}
+            </div>
+        );
+    else if (type === 5)
+        return (
+            <div
+                className={`flex flex-col justify-between items-center gap-x-3 gap-y-3 ${className}`}
             >
                 {children}
             </div>
