8721690b3da5b194da1f2e3b63c16879956d9cfa
Talha Mujahid <htmujahid@gmail.com>
2022-12-03 16:42:01 +0500

    initial commit

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,51 +14,15 @@ export default NextAuth({
     session: {
         jwt: true,
     },
+    
     pages: {
         signIn: "/auth/signin",
-        signOut: "/auth/signin",
-        error: "/auth/signin",
-        verifyRequest: "/auth/signin",
-        newUser: "/auth/signin",
     },
+    
     adapter: MongoDBAdapter(client.connect()),
 
     // Configure one or more authentication providers
     providers: [
-        CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: "Credentials",
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
-            async authorize(credentials, req) {
-                // You need to provide your own logic here that takes the credentials
-                // submitted and returns either a object representing a user or value
-                // that is false/null if the credentials are invalid.
-                // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-                // You can also use the `req` object to obtain additional parameters
-                // (i.e., the request IP address)
-                const connect = await client.connect();
-                const db = connect.db();
-                const users = db.collection("users");
-                const user = await users.findOne({ email: credentials.email });
-                if (!user) {
-                    client.close();
-                    throw new Error("No user found!");
-                }
-                const isValid = await verifyPassword(
-                    credentials.password,
-                    user.password
-                );
-                if (!isValid) {
-                    client.close();
-                    throw new Error("Could not log you in!");
-                }
-                client.close();
-                return { email: user.email };
-            },
-        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
