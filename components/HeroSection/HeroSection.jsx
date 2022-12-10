8ee21a8abdc52ee8c50c5a82eca8a19a98d7d93d
Talha Mujahid <htmujahid@gmail.com>
2022-12-11 04:20:48 +0500

    initial commit

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -16,7 +16,7 @@ function HeroSection() {
                         teams achieve their goals.
                     </p>
                     <Link
-                        href="#"
+                        href="/auth/signin"
                         className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 duration-100"
                     >
                         Get started
