62035759515efdbe91a388efc8b5af12379a1459
htmujahid <htmujahid@gmail.com>
2022-12-11 13:19:15 +0500

    initial commit

--- components/Appbar/AppbarWrapper.jsx
+++ components/Appbar/AppbarWrapper.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import Sidebar from "@/components/Appbar/Sidebar";
 import Navbar from "./Navbar";
 function AppbarWrapper({ children }) {
-    if (typeof window !== "undefined") {
-        document.body.style.overflow = "hidden";
-    }
     return (
         <div className="flex">
             <Sidebar />
-            <div className="flex-1 overflow-x-scroll ">
+            <div className="flex-1 overflow-x-auto ">
                 <Navbar />
                 <div className="h-screen overflow-y-auto pb-10">{children}</div>
             </div>
