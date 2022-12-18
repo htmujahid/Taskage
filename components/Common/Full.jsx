9ea41df08addcd401f152d6ceaa6c797f0ec91fb
htmujahid <htmujahid@gmail.com>
2022-12-18 14:25:06 +0500

    initial commit

--- components/Common/Full.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Full({ children, className }) {
-    return <div className={`w-full ${className}`}>{children}</div>;
-}
-
-export default Full;
--- components/Typography/Paragraph.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Paragraph({ className, children }) {
-    return (
-        <p className={`text-sm text-gray-500 mt-3 ${className}`}>{children}</p>
-    );
-}
-
-export default Paragraph;
